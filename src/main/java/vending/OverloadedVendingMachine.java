d2097f80b9ff4084b32c49a4b01622c84f4b23da
Salizwa27 <salizwamayekiso27@gmail.com>
2021-07-23 15:29:28 +0200

    added constructor and if statement on buy method

--- src/main/java/vending/OverloadedVendingMachine.java
+++ src/main/java/vending/OverloadedVendingMachine.java
@@ -4,16 +4,39 @@ import vending.product.*;
 
 public class OverloadedVendingMachine {
 
+    private int softDrinkQty;
+    private int saltySnacksQty;
+    private int chocolatesQty;
+
+    // add a constructor like this to you OverloadedVendingMachine class
+    OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty){
+        // set the stockLevel instance variables for each product in the constructor
+
+        this.softDrinkQty = softDrinkQty;
+        this.saltySnacksQty = saltySnacksQty;
+        this.chocolatesQty = chocolatesQty;
+    }
 
     void buy(SoftDrink softdrink){
 
+        if (softdrink != null){
+            softDrinkQty -- ;
+        }
     }
 
     void buy(SaltySnack saltySnack){
 
+        if (saltySnack != null){
+            saltySnacksQty -- ;
+        }
+
     }
 
     void buy(Chocolate chocolate){
+
+        if (chocolate != null){
+            chocolatesQty -- ;
+        }
 
     }
 
